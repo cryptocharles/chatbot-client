edc1a436eb768ff1048f550c7e1752c418f0488d
Charles <cryptocharles@gmail.com>
2016-04-06 23:07:00 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='tipbot',
+    name='chatbot',
     version='1',
-    py_modules=['tipbot'],
+    py_modules=['chatbot'],
         install_requires=[
             'Click',
     ],
     entry_points='''
         [console_scripts]
-        tip=tipbot:cli
-        tip-lasts=tipbot:last
+        chat=chatbot:cli
     ''',
 )
